3ec835afb856f8baef2a484851aeaaa17fd349b4
Trislovesu <159201578+Trislovesu@users.noreply.github.com>
2026-05-19 23:18:16 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'dm-assistant-v1';
+const STATIC = ['./','./index.html','./manifest.json','./icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(STATIC)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if(e.request.url.includes('api.groq.com') || e.request.url.includes('fonts.googleapis.com')){
+    e.respondWith(fetch(e.request));
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
+      if(res.ok){caches.open(CACHE).then(c => c.put(e.request, res.clone()));}
+      return res;
+    }))
+  );
+});
